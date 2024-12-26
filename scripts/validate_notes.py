af118135f5c4286eb730032db6babfc811b9fb0a
Sean Dewantoro <seanandrewvalentino@gmail.com>
2024-12-26 22:56:54 +1000

    Clean Up `validate_note_structure`
    Improved readability and organization of the code for validating note
    structures. The implementation itself is changed slightly but still
    serves as intended. Constants in scripts/constants/note.py such as
    indeces and lengths are all included explicitly and are complete.
    
    Also fixed a minor bug where multiple additionals aren't checked.
    
    Main changes are in `scripts/validate_notes.py` and
    `scripts/constants/note.py`. The .gitignore file is modified to now
    ignore all __pycache__/ directories regardless where it is.

--- scripts/validate_notes.py
+++ scripts/validate_notes.py
@@ -1,31 +1,41 @@
 from constants.note import *
 
 def validate_note_structure(note_structure: str):
-    splitted = note_structure.split(STRUCTURE_SEPARATOR)
+    splitted = note_structure.split(SEPARATOR_NOTE_STRUCTURE)
 
     if not len(splitted) in VALID_STRUCTURE_LENGTHS:
         return False
 
-    note, duration = splitted[:2]
+    note, duration = splitted[:NOTE_STRUCTURE_LEN_SHORT]
 
-    if len(note) == 2:
-        is_note_valid = note[0] in VALID_NOTES
-        is_octave_valid = note[1].isnumeric() and OCTAVE_MIN <= int(note[1]) <= OCTAVE_MAX
-        if not is_note_valid or not is_octave_valid:
-            return False
-    else:
-        is_note_valid = note[0] in VALID_NOTES
-        is_accidental_valid = note[1] in ACCIDENTAL_FLAT + ACCIDENTAL_SHARP
-        is_octave_valid = note[2].isnumeric() and OCTAVE_MIN <= int(note[2]) <= OCTAVE_MAX
-        if not is_note_valid or not is_accidental_valid or not is_octave_valid:
+    # Check accidental
+    if len(note) == NOTE_STRUCTURE_LEN_LONG:
+        is_accidental_valid = note[NOTE_IDX_ACCIDENTAL] in ACCIDENTAL_FLAT + ACCIDENTAL_SHARP
+        if not is_accidental_valid:
             return False
 
+    # Check note symbol
+    is_symbol_valid = note[NOTE_IDX_SYMBOL] in VALID_SYMBOLS
+    if not is_symbol_valid:
+        return False
+
+    # Check octave
+    octave = note[NOTE_IDX_OCTAVE_SHORT if len(note) == NOTE_LEN_SHORT else NOTE_IDX_OCTAVE_LONG]
+    is_octave_valid = octave.isnumeric() and OCTAVE_MIN <= int(octave) <= OCTAVE_MAX
+    if not is_octave_valid:
+        return False
+
+    # Check duration
     if not duration.isnumeric() or not int(duration) in VALID_DURATIONS:
         return False
 
-    if len(splitted) == 2:
+    # No additionals
+    if len(splitted) == NOTE_STRUCTURE_LEN_SHORT:
         return True
 
-    additional = splitted[2]
-
-    return additional in VALID_ADDITIONALS
+    # Check additionals
+    additionals = splitted[NOTE_STRUCTURE_IDX_ADDITIONALS]
+    all_additionals_unique = len(set(additionals)) == len(additionals)
+    all_additionals_valid = all(additional in VALID_ADDITIONALS for additional in additionals)
+    no_slur_overlap = not (ADDITIONAL_SLUR_BEGIN in additionals and ADDITIONAL_SLUR_END in additionals)
+    return all_additionals_unique and all_additionals_valid and no_slur_overlap
