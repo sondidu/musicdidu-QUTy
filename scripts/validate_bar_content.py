db4a69aa43d2c030a29c05e05a2461eff59b7bda
Sean Dewantoro <seanandrewvalentino@gmail.com>
2024-12-30 00:17:19 +1000

    Rename constants notes.py
    Renamed and reorganized constants of `scripts/constants/notes.py`.
    Things are now grouped better, and sorted alphabetically. Only
    `scripts/validate_bar_content.py` that needs to reflect to this change.
    
    Some are intentionally not sorted alphabetically because some is better
    sorted logically. Only `DURATIONS_` are not sorted alphabetically.

--- scripts/validate_bar_content.py
+++ scripts/validate_bar_content.py
@@ -1,26 +1,26 @@
 from constants.notes import *
 
 def validate_note_structure(note_structure: str):
-    splitted = note_structure.split(SEPARATOR_NOTE_STRUCTURE)
+    splitted = note_structure.split(NOTESTRUCT_SEP)
 
-    if not len(splitted) in VALID_STRUCTURE_LENGTHS:
+    if not len(splitted) in VALID_NOTESTRUCT_LENS:
         return False
 
-    note, duration = splitted[:NOTE_STRUCTURE_LEN_SHORT]
+    note, duration = splitted[:NOTESTRUCT_EXPLEN_SHORT]
 
     # Check accidental
-    if len(note) == NOTE_STRUCTURE_LEN_LONG:
-        is_accidental_valid = note[NOTE_IDX_ACCIDENTAL] in ACCIDENTAL_FLAT + ACCIDENTAL_SHARP
+    if len(note) == NOTESTRUCT_EXPLEN_LONG:
+        is_accidental_valid = note[NOTE_IDX_ACCIDENTAL] in NOTE_ACCIDENTAL_FLAT + NOTE_ACCIDENTAL_SHARP
         if not is_accidental_valid:
             return False
 
     # Check note symbol
-    is_symbol_valid = note[NOTE_IDX_SYMBOL] in VALID_SYMBOLS
+    is_symbol_valid = note[NOTE_IDX_SYMBOL] in VALID_SYMS
     if not is_symbol_valid:
         return False
 
     # Check octave
-    octave = note[NOTE_IDX_OCTAVE_SHORT if len(note) == NOTE_LEN_SHORT else NOTE_IDX_OCTAVE_LONG]
+    octave = note[NOTE_IDX_OCTAVE_SHORT if len(note) == NOTE_EXPLEN_SHORT else NOTE_IDX_OCTAVE_LONG]
     is_octave_valid = octave.isnumeric() and NOTE_OCTAVE_MIN <= int(octave) <= NOTE_OCTAVE_MAX
     if not is_octave_valid:
         return False
@@ -30,42 +30,42 @@ def validate_note_structure(note_structure: str):
         return False
 
     # No additionals
-    if len(splitted) == NOTE_STRUCTURE_LEN_SHORT:
+    if len(splitted) == NOTESTRUCT_EXPLEN_SHORT:
         return True
 
     # Check additionals
-    additionals = splitted[NOTE_STRUCTURE_IDX_ADDITIONALS]
+    additionals = splitted[NOTESTRUCT_IDX_ADDITIONALS]
     all_additionals_unique = len(set(additionals)) == len(additionals)
     all_additionals_valid = all(additional in VALID_ADDITIONALS for additional in additionals)
     no_slur_overlap = not (ADDITIONAL_SLUR_BEGIN in additionals and ADDITIONAL_SLUR_END in additionals)
     return all_additionals_unique and all_additionals_valid and no_slur_overlap
 
 def validate_break_structure(break_structure: str):
-    splitted = break_structure.split(SEPARATOR_BREAK)
+    splitted = break_structure.split(BREAKSTRUCT_SEP)
 
-    if len(splitted) != BREAK_STRUCTURE_LEN:
+    if len(splitted) != BREAKSTRUCT_EXPLEN:
         return False
 
     break_char, duration = splitted
 
-    is_valid_break = break_char == SYMBOL_BREAK
+    is_valid_break = break_char == BREAK_SYM
     is_valid_duration = duration.isnumeric() and int(duration) in VALID_DURATIONS
     return is_valid_break and is_valid_duration
 
 def validate_tuplet(tuplet: str):
-    if tuplet.count(TUPLET_RATIO) != TUPLET_RATIO_COUNT_EXPECTED:
-        return False
-
     if tuplet[-1] != TUPLET_CLOSE:
         return False
 
-    # '(' not found
+    # The '(' char is not found
     if (tuplet_open_idx := tuplet.find(TUPLET_OPEN)) == -1:
         return False
 
     # Validating tuplet definition
     tuplet_defs = tuplet[:tuplet_open_idx]
-    def_splitted = tuplet_defs.split(TUPLET_RATIO)
+    def_splitted = tuplet_defs.split(TUPLET_SEP_DEF)
+
+    if len(def_splitted) != TUPLET_EXPDEFS:
+        return False
 
     if not all(tuplet_def.isnumeric() for tuplet_def in def_splitted):
         return False
@@ -76,5 +76,5 @@ def validate_tuplet(tuplet: str):
 
     # Validating individual notes in tuplet
     tuplet_notes = tuplet[tuplet_open_idx + 1:-1]
-    notes_splitted = tuplet_notes.split(SEPARATOR_TUPLET_NOTE)
+    notes_splitted = tuplet_notes.split(TUPLET_SEP_NOTE)
     return all(validate_note_structure(note_structure) for note_structure in notes_splitted)
