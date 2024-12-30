6589f1b9ab056b0b9871e2f4f476323f8b368e35
Sean Dewantoro <seanandrewvalentino@gmail.com>
2024-12-30 15:20:08 +1000

    Implement bar and setting validation
    Implemented `validate_bar` and `validate_setting_block` in
    `scripts/avlidate_blocks.py`. As of now, these two functions only
    return whether a given block is valid. No errors are shown yet. Errors
    might soon be implemented.

--- scripts/validate_blocks.py
+++ scripts/validate_blocks.py
@@ -1,6 +1,38 @@
+from constants.notes import NOTE_SEP, TUPLET_CLOSE, BREAK_SYM
+from constants.setting_fields import FIELD_SEP
+from validate_bar_content import *
+from validate_setting_content import validate_setting_field
 
 def validate_bar(bar: str):
-    pass
+    content = bar[1:-1] # Extract content
+    notes = content.split(NOTE_SEP)
+
+    for note in notes:
+        # Empty note
+        if note == '':
+            return False
+        # Tuplets
+        elif note[-1] == TUPLET_CLOSE:
+            if validate_tuplet(note) == False:
+                return False
+        # Breaks
+        elif note[0] == BREAK_SYM:
+            if validate_break_structure(note) == False:
+                return False
+        # Notes
+        else:
+            if validate_note_structure(note) == False:
+                return False
+
+    return True
 
 def validate_setting_block(setting_block: str):
-    pass
+    content = setting_block[1:-1] # Extract content
+    fields = content.split(FIELD_SEP)
+
+    for field in fields:
+        field_stripped = field.strip()
+        if validate_setting_field(field_stripped) == False:
+            return False
+
+    return True
